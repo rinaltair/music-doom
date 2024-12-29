0b79b0c18c9093307d7e8e70623fd2136ffb8bea
rinal <rinalditoryanari@gmail.com>
2024-12-29 16:39:28 +0700

    dep: add megajs and multer

--- src/config/config.js
+++ src/config/config.js
@@ -23,6 +23,8 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    MEGA_EMAIL: Joi.string().required().description('email for credentials MEGA'),
+    MEGA_PASSWORD: Joi.string().required().description('password for credentials MEGA'),
   })
   .unknown();
 
@@ -62,7 +64,7 @@ module.exports = {
     from: envVars.EMAIL_FROM,
   },
   mega: {
-    email: process.env.MEGA_EMAIL,
-    password: process.env.MEGA_PASSWORD,
+    email: envVars.MEGA_EMAIL,
+    password: envVars.MEGA_PASSWORD,
   },
 };
