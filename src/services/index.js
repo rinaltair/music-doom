f1ab041ec54dbac41b32c8328f27317f7b529fa3
rinal <rinalditoryanari@gmail.com>
2025-01-02 15:50:05 +0700

    refactor: seperate controller's service and non controller'service

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,9 @@
+// Controller
 module.exports.authService = require('./auth.service');
-module.exports.emailService = require('./email.service');
-module.exports.tokenService = require('./token.service');
 module.exports.userService = require('./user.service');
-module.exports.megaService = require('./mega.service');
 module.exports.songService = require('./song.service');
+
+// Non Contorller
+module.exports.megaService = require('./mega.service');
+module.exports.tokenService = require('./token.service');
+module.exports.emailService = require('./email.service');
