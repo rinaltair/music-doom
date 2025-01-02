f1cf014d3ea47da31f26b20dcde8b3632343affa
rinal <rinalditoryanari@gmail.com>
2025-01-02 08:57:02 +0700

    fix: change the naming file song

--- src/services/song.service.js
+++ src/services/song.service.js
@@ -6,7 +6,7 @@ const random = require('../utils/random');
 
 const uploadSong = async (song, file) => {
   // Upload file to cloud storage
-  const filename = random.randFileName(file.originalname);
+  const filename = `${random.randFileName()}.mp3`;
   const result = await megaService.uploadFile('song', file, filename);
   if (!result) {
     throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'File upload failed');
