7b76a60639a859a0d8529061dd5b825e215a8788
rinal <rinalditoryanari@gmail.com>
2025-01-05 21:57:12 +0700

    feat: add update by id and get by id in crud

--- src/services/crud.service.js
+++ src/services/crud.service.js
@@ -4,8 +4,15 @@ const crud = (model) => ({
     await object.save();
     return object;
   },
-  async getAll(data) {
-    return await model.find(data);
+
+  async updateById(id, data) {
+    const object = await model.findOneAndUpdate({ _id: id }, data, { new: true });
+    return object;
+  },
+
+  async getById(id) {
+    const object = await model.findById(id);
+    return object;
   },
 });
 
