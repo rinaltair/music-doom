ce1b229b02afceddd6c4289b204cdcda551a9542
rinal <rinalditoryanari@gmail.com>
2025-01-05 09:01:50 +0700

    feat: add the randomFilename soon will delete the randFileName

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,10 +1,20 @@
 const { v4: uuid } = require('uuid');
+const path = require('node:path');
 
 const randFileName = () => {
   const random = uuid();
   return random;
 };
 
+const randomFilename = (file) => {
+  const extension = path.extname(file.originalname).toLowerCase();
+  const random = uuid();
+
+  const filename = `${random}${extension}`;
+  return filename;
+};
+
 module.exports = {
   randFileName,
+  randomFilename,
 };
