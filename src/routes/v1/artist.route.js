5d1037ea51e823f2d037e6cdf515d1030b65a253
rinal <rinalditoryanari@gmail.com>
2025-01-06 10:49:54 +0700

    refactor: clean the route

--- src/routes/v1/artist.route.js
+++ src/routes/v1/artist.route.js
@@ -6,18 +6,16 @@ const validate = require('../../middlewares/validate');
 const { artistValidation } = require('../../validations');
 const { artistController } = require('../../controllers');
 
+const noFile = multer.p.none();
+const profile = multer.p.single('profile');
+
 const router = express.Router();
 
-router.route('/').get(multer.p.none(), artistController.getArtists);
+router.get('/', noFile, artistController.getArtists);
+router.post('/create', profile, validate(artistValidation.createArtist), artistController.createArtist);
 
-router
-  .route('/create')
-  .post(multer.p.single('profile'), validate(artistValidation.createArtist), artistController.createArtist);
-
-router.route('/:id/').get(multer.p.none(), validate(artistValidation.getArtistByID), artistController.getArtistById);
-router
-  .route('/:id/update')
-  .post(multer.p.single('profile'), validate(artistValidation.updateArtist), artistController.updateArtist);
-router.route('/:id/delete').post(multer.p.none(), validate(artistValidation.getArtistByID), artistController.deleteArtist);
+router.get('/:id/', noFile, validate(artistValidation.getArtistByID), artistController.getArtistById);
+router.post('/:id/update', profile, validate(artistValidation.updateArtist), artistController.updateArtist);
+router.post('/:id/delete', noFile, validate(artistValidation.getArtistByID), artistController.deleteArtist);
 
 module.exports = router;
