b1ce7e28b48f4e97804025e267cfc228b22edf1d
rinal <rinalditoryanari@gmail.com>
2025-01-19 11:56:56 +0700

    docs: add documentation for crud service

--- src/services/crud.service.js
+++ src/services/crud.service.js
@@ -1,8 +1,7 @@
 const random = require('../utils/random');
 const megaService = require('./mega.service');
 const ApiError = require('../utils/ApiError');
-const httpStatus = require('http-status');
-const { updateByIdWithPhoto } = require('./artist.service');
+
 const crud = (model) => ({
   async create(data) {
     const object = new model(data);
@@ -10,11 +9,21 @@ const crud = (model) => ({
     return object;
   },
 
+  /**
+   * Create an object with a new photo
+   * @param {Object} data - The data to create the object with
+   * @param {Object} file - The photo file to upload
+   * @param {string} propertyFile - The property name for the photo URL
+   * @returns {Promise<Object>} - The created object
+   * @throws {ApiError} - If the object cannot be created or the photo cannot be uploaded
+   */
   async createwithPhoto(data, file, propertyFile) {
+    // Upload the photo with random filename
     const filename = random.randomFilename(file);
     await megaService.uploadFile('picture', file, filename);
 
     try {
+      // Create and save the object
       let object = new model({ ...data, [propertyFile]: filename });
       object = await object.save();
       return object;
@@ -24,11 +33,27 @@ const crud = (model) => ({
     }
   },
 
+  /**
+   * Update an object by id
+   * @param {ObjectId} id - The id of the object to update
+   * @param {Object} data - The data to update the object with
+   * @returns {Promise<Object>} - The updated object
+   * @throws {ApiError} - If the object cannot be found or updated
+   */
   async updateById(id, data) {
     const object = await model.findOneAndUpdate({ _id: id }, data, { new: true });
     return object;
   },
 
+  /**
+   * Update an object by id with a new photo
+   * @param {ObjectId} id - The id of the object to update
+   * @param {Object} updateBody - The data to update the object with
+   * @param {Object} file - The photo file to upload
+   * @param {string} photoProperty - The property name for the photo URL
+   * @returns {Promise<Object>} - The updated object
+   * @throws {ApiError} - If the object cannot be found or updated
+   */
   async updateByIdWithPhoto(id, updateBody, file, photoProperty) {
     const object = await model.findOneAndUpdate({ _id: id }, updateBody, { new: true }).catch((error) => {
       throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, `Unable to find ${model.modelName}`);
@@ -50,16 +75,32 @@ const crud = (model) => ({
     }
   },
 
+  /**
+   * Get an object by id
+   * @param {ObjectId} id - The id of the object to get
+   * @returns {Promise<Object>} - The object
+   * @throws {ApiError} - If the object cannot be found
+   */
   async getById(id) {
     const object = await model.findById(id);
     return object;
   },
 
+  /**
+   * Get all objects
+   * @returns {Promise<Object[]>} - All objects
+   */
   async getAll() {
     const objects = await model.find();
     return objects;
   },
 
+  /**
+   * Delete an object by id
+   * @param {ObjectId} id - The id of the object to delete
+   * @returns {Promise<Object>} - The deleted object
+   * @throws {ApiError} - If the object cannot be found
+   */
   async deleteById(id) {
     const object = await model.findByIdAndDelete(id);
     return object;
