db348d9c83a24d76fffd4d5bbd1d159e519dc45f
rinal <rinalditoryanari@gmail.com>
2025-01-19 10:25:56 +0700

    feat: adding file schema for validaiton process

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -3,17 +3,31 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 
+/**
+ * Validate request body, query, and params against a schema.
+ * @param {Object} schema - Joi schema.
+ * @param {Object} schema.body - Schema for request body.
+ * @param {Object} schema.query - Schema for query parameters.
+ * @param {Object} schema.params - Schema for route parameters.
+ * @return {Function} - Express middleware function.
+ */
 const validate = (schema) => (req, res, next) => {
-  const validSchema = pick(schema, ['params', 'query', 'body']);
+  // Extract the relevant schema parts for validation
+  const validSchema = pick(schema, ['params', 'query', 'body', 'file']);
   const object = pick(req, Object.keys(validSchema));
+
+  // Validate the request data against the schema
   const { value, error } = Joi.compile(validSchema)
     .prefs({ errors: { label: 'key' }, abortEarly: false })
     .validate(object);
 
+  // If validation fails, create an error message and pass it to the next middleware
   if (error) {
     const errorMessage = error.details.map((details) => details.message).join(', ');
     return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
   }
+
+  // Replace req query/body/params with the validated values
   Object.assign(req, value);
   return next();
 };
